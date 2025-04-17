36f0cddc31c3bc45e605ca1c2831a704335ddfea
Sofiane MEKSI <sof@meksi.me>
2025-04-17 15:28:46 +0200

    refactor(env): Update environment variable handling and improve content import
    - Add script for tracking system
    - Change `NEXT_PUBLIC_CALENDLY_URL` to `ASTRO_PUBLIC_CALENDLY_URL` in the README and components.
    - Introduce `getEnvVar` utility for centralized environment variable access.
    - Update imports in various components to use the new content utility for better organization.

---
+++ src/utils/env.ts
@@ -0,0 +1,9 @@
+const env = {
+    ASTRO_PUBLIC_CALENDLY_URL: process.env.ASTRO_PUBLIC_CALENDLY_URL || import.meta.env.ASTRO_PUBLIC_CALENDLY_URL || '',
+    ACKEE_SERVER_URL: process.env.ACKEE_SERVER_URL || import.meta.env.ACKEE_SERVER_URL || '',
+    ACKEE_TRACKER_URL: process.env.ACKEE_TRACKER_URL || import.meta.env.ACKEE_TRACKER_URL || '',
+    ACKEE_DOMAIN_ID: process.env.ACKEE_DOMAIN_ID || import.meta.env.ACKEE_DOMAIN_ID || ''
+}
+export function getEnvVar(key: keyof typeof env): string {
+    return env[key];
+}
